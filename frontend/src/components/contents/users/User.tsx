33f702db56dca90baad18498e8c248742b556979
Lê Thanh Toàn <toan20040331@gmail.com>
2025-04-03 14:59:18 +0700

    finish table user

--- frontend/src/components/contents/users/User.tsx
+++ frontend/src/components/contents/users/User.tsx
@@ -1,5 +1,18 @@
+import { Button } from "antd";
+import TableUser from "./tables/TableUser";
+
 const User = () => {
-  return <div>User</div>;
+  return (
+    <div className="w-full flex flex-col">
+      <div className="w-full flex flex-col border-1 border-[#f0f0f0] rounded-tr-[10px] rounded-tl-[10px]">
+        <div className="w-full h-[60px] border-b-1 border-[#f0f0f0] flex flex-row justify-between items-center px-[10px]">
+          <span className="font-bold text-[20px]">USER</span>
+          <Button type="primary">Create user</Button>
+        </div>
+        <TableUser />
+      </div>
+    </div>
+  );
 };
 
 export default User;
---
+++ frontend/src/components/contents/users/tables/TableUser.tsx
@@ -0,0 +1,347 @@
+import React, { useRef, useState } from "react";
+import {
+  SearchOutlined,
+  MoreOutlined,
+  EyeOutlined,
+  EditOutlined,
+  DeleteOutlined,
+} from "@ant-design/icons";
+import type {
+  InputRef,
+  TableColumnsType,
+  TableColumnType,
+  TableProps,
+} from "antd";
+import { Button, Input, Space, Table, Dropdown } from "antd";
+import type { FilterDropdownProps } from "antd/es/table/interface";
+import Highlighter from "react-highlight-words";
+
+interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
+
+type DataIndex = keyof DataType;
+type TableRowSelection<T extends object = object> =
+  TableProps<T>["rowSelection"];
+
+const data: DataType[] = [
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+  },
+  {
+    key: "2",
+    name: "Joe Black",
+    age: 42,
+    address: "London No. 1 Lake Park",
+  },
+  {
+    key: "3",
+    name: "Jim Green",
+    age: 32,
+    address: "Sydney No. 1 Lake Park",
+  },
+  {
+    key: "4",
+    name: "Jim Red",
+    age: 32,
+    address: "London No. 2 Lake Park",
+  },
+  {
+    key: "5",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+  },
+  {
+    key: "6",
+    name: "Joe Black",
+    age: 42,
+    address: "London No. 1 Lake Park",
+  },
+  {
+    key: "7",
+    name: "Jim Green",
+    age: 32,
+    address: "Sydney No. 1 Lake Park",
+  },
+  {
+    key: "8",
+    name: "Jim Red",
+    age: 32,
+    address: "London No. 2 Lake Park",
+  },
+  {
+    key: "9",
+    name: "Jim Green",
+    age: 32,
+    address: "Sydney No. 1 Lake Park",
+  },
+  {
+    key: "10",
+    name: "Jim Red",
+    age: 32,
+    address: "London No. 2 Lake Park",
+  },
+  {
+    key: "11",
+    name: "Jim Green",
+    age: 32,
+    address: "Sydney No. 1 Lake Park",
+  },
+  {
+    key: "12",
+    name: "Jim Red",
+    age: 32,
+    address: "London No. 2 Lake Park",
+  },
+];
+
+const TableUser: React.FC = () => {
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef<InputRef>(null);
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: FilterDropdownProps["confirm"],
+    dataIndex: DataIndex
+  ) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchText("");
+  };
+
+  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  const rowSelection: TableRowSelection<DataType> = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+    selections: [
+      Table.SELECTION_ALL,
+      Table.SELECTION_INVERT,
+      Table.SELECTION_NONE,
+      {
+        key: "odd",
+        text: "Select Odd Row",
+        onSelect: (changeableRowKeys) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
+            if (index % 2 !== 0) {
+              return false;
+            }
+            return true;
+          });
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      },
+      {
+        key: "even",
+        text: "Select Even Row",
+        onSelect: (changeableRowKeys) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
+            if (index % 2 !== 0) {
+              return true;
+            }
+            return false;
+          });
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      },
+    ],
+  };
+
+  const getColumnSearchProps = (
+    dataIndex: DataIndex
+  ): TableColumnType<DataType> => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() =>
+            handleSearch(selectedKeys as string[], confirm, dataIndex)
+          }
+          style={{ marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() =>
+              handleSearch(selectedKeys as string[], confirm, dataIndex)
+            }
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}>
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}>
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText((selectedKeys as string[])[0]);
+              setSearchedColumn(dataIndex);
+            }}>
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}>
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    filterDropdownProps: {
+      onOpenChange(open) {
+        if (open) {
+          setTimeout(() => searchInput.current?.select(), 100);
+        }
+      },
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const columns: TableColumnsType<DataType> = [
+    {
+      title: "ID",
+      dataIndex: "key",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      width: "30%",
+      ...getColumnSearchProps("name"),
+      sorter: (a, b) => a.name.localeCompare(b.name),
+      sortDirections: ["descend", "ascend"],
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+      width: "20%",
+      ...getColumnSearchProps("age"),
+      sorter: (a, b) => a.age - b.age,
+      sortDirections: ["descend", "ascend"],
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+      ...getColumnSearchProps("address"),
+      sorter: (a, b) => a.address.localeCompare(b.address),
+      sortDirections: ["descend", "ascend"],
+    },
+    {
+      title: "Actions",
+      key: "actions",
+      render: () => (
+        <Dropdown
+          menu={{
+            items: [
+              {
+                key: "1",
+                icon: <EyeOutlined style={{ color: "#1890ff" }} />,
+                label: <span style={{ color: "#1890ff" }}>View Details</span>,
+              },
+              {
+                key: "2",
+                icon: <EditOutlined style={{ color: "#52c41a" }} />,
+                label: <span style={{ color: "#52c41a" }}>Edit</span>,
+              },
+              {
+                key: "3",
+                icon: <DeleteOutlined style={{ color: "#ff4d4f" }} />,
+                label: <span style={{ color: "#ff4d4f" }}>Delete</span>,
+              },
+            ],
+          }}
+          trigger={["hover"]}>
+          <Button
+            type="text"
+            icon={
+              <MoreOutlined style={{ fontSize: "20px", color: "#595959" }} />
+            }
+            style={{
+              borderRadius: "50%",
+              width: "32px",
+              height: "32px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              transition: "all 0.3s",
+            }}
+            className="hover:bg-[#f0f0f0]"
+          />
+        </Dropdown>
+      ),
+    },
+  ];
+
+  return (
+    <Table<DataType>
+      columns={columns}
+      dataSource={data}
+      rowSelection={rowSelection}
+      pagination={{
+        pageSize: 5,
+      }}
+    />
+  );
+};
+
+export default TableUser;
